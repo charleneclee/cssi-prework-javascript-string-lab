987cbb9575a826e259dc9d9cafc751632c02c8fd
charleneclee <cclee@google.com>
2017-05-08 13:18:58 -0700

    finished silly string lab

--- js/encoder.js
+++ js/encoder.js
@@ -1,10 +1,5 @@
 var encode = function(userString){
-  //1. Add code to turn string to lowercase
-  
-  // 2. Add code to change each instance of a, e, i, o to the appropriate number based on l33n rules
-
-  // 3. Add code to replace spaces with dollar signs.
-
-  //This line assigns the variable encode to the value userString
+  var userString = userString.toLowerCase()
+  var userString = "userString".replace(/a/g, "4").replace(/e/g, "3").replace(/i/g, "1").replace(/o/g, "0").replace(/ /g, "$")
   return userString
 }
